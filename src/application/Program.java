9a95a423182b2255ef61ba12776b7a5e82b9d05c
Rui Pereira <rui6.319@hotmail.com>
2024-05-17 10:32:12 +0100

    findById implementatio

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,29 +1,19 @@
 package application;
 
-import java.util.Date;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import db.DB;
-import db.DbException;
-import db.DbIntegrityException;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "Books");
-		Seller  seller = new Seller(21,"Bob", "bob@gmail", new Date(), 3000.0, obj);
+		//Department obj = new Department(1, "Books");
+		//Seller  seller = new Seller(21,"Bob", "bob@gmail", new Date(), 3000.0, obj);
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
 		
