c0ede048e24a2e702810ea38fd16bb6bc024af51
Rui Pereira <rui6.319@hotmail.com>
2024-05-22 12:47:07 +0100

    update implementation

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -10,8 +10,8 @@ public interface SellerDao {
 	void insert(Seller obj);
 	void update(Seller obj);
 	void deleteById(Seller obj);
-	Seller findById(Integer id);
 	List<Seller> findAll();
 	List<Seller> findByDepartment(Department department);
+	Seller findById(Integer id);
 
 }
